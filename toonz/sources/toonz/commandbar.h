b9674e21048315028b777ee9186a4feebcbc313b
Rodney <rodney.baker@gmail.com>
2026-08-15 18:25:57 -0500

    Add command bar orientation state

--- toonz/sources/toonz/commandbar.h
+++ toonz/sources/toonz/commandbar.h
@@ -5,6 +5,7 @@
 
 #include <memory>
 
+#include "saveloadqsettings.h"
 #include "toonz/txsheet.h"
 #include "toonzqt/keyframenavigator.h"
 
@@ -19,14 +20,18 @@ class QAction;
 // CommandBar
 //-----------------------------------------------------------------------------
 
-class CommandBar : public QToolBar {
+class CommandBar : public QToolBar, public SaveLoadQSettings {
   Q_OBJECT
 protected:
   bool m_isCollapsible;
+  bool m_isXsheetToolbar;
 
 public:
   CommandBar(QWidget *parent = 0, Qt::WindowFlags flags = Qt::WindowFlags(),
              bool isCollapsible = false, bool isXsheetToolbar = false);
+
+  void save(QSettings &settings) const override;
+  void load(QSettings &settings) override;
 
 signals:
   void updateVisibility();
@@ -38,6 +43,7 @@ protected:
 
 protected slots:
   void doCustomizeCommandBar();
+  void onOrientationChanged(Qt::Orientation orientation);
 };
 
 #endif  // COMMANDBAR_H
