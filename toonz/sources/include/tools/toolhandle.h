567761e613c7e3922512fedd983216bcfd89265c
Rodney <rodney.baker@gmail.com>
2026-08-31 09:51:04 -0500

    Fix tool handle export macro

--- toonz/sources/include/tools/toolhandle.h
+++ toonz/sources/include/tools/toolhandle.h
@@ -18,7 +18,7 @@ class QString;
 #undef DVVAR
 #ifdef TNZTOOLS_EXPORTS
 #define DVAPI DV_EXPORT_API
-#define DVVAR DV_IMPORT_API
+#define DVVAR DV_EXPORT_VAR
 #else
 #define DVAPI DV_IMPORT_API
 #define DVVAR DV_IMPORT_VAR
